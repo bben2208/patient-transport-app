5dbca1343a912e894c096adfaee0facd5f0c7e11
Benjamin Bognar <benjaminbognar@MacBook-Pro-2.local>
2025-04-07 23:07:06 +0100

    models

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const passport = require("passport");
-const User = require("../models/user");
+const User = require("../model/user");
 const Transport = require("../model/transport"); // Import the Transport model
 const { ensureAuthenticated } = require("../middleware/auth");
 const router = express.Router();
